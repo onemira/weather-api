94a2a42aa84b12fef9db7e4357ae86cdb5004db7
onemira <mirabulgogi@gmail.com>
2019-03-11 14:02:50 -0400

    updated

--- public/main.js
+++ public/main.js
@@ -1,7 +1,17 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let currentWeather = {}
+
+const forecast = () => {
+  let location = document.querySelector('.location-term').value
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=7d5fd7008df38afb95ddf8cdd2456700&units=imperial')
+    .then(resp => {
+      return resp.json()
+    })
+    .then(weather => {
+      // console.log(weather)
+      currentWeather = weather
+      document.querySelector('.result').textContent = '" ' + currentWeather.name + ' is ' + currentWeather.weather[0].main + '" '
+      console.log(currentWeather.weather[0].main)
+    })
 }
 
-document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.search').addEventListener('click', forecast)
